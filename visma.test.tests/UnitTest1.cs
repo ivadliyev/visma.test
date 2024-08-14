34dec262da4957c3b8854b45475fe392747e448c
Adilet <adilet@diga.pt>
2024-08-14 19:00:06 +0100

    Initial commit

---
+++ visma.test.tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace visma.test.tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
